c64e30f63254183ca2ace8930f9d4fccc82f4489
Your Name <lazaro.domiciano@gmail.com>
2025-06-17 19:59:30 -0300

    chore: Atualiza texto de direitos autorais no rodapé.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
 <footer class="bg-secondary-500 text-white p-4 text-center fixed bottom-0 w-full">
-    © {new Date().getFullYear()} Escola Divertida. Desenvolvido por lazaro.domiciano.
+    © {new Date().getFullYear()} Escola Divertida. Desenvolvido por lazaro.domiciano e algumas IAs.
 </footer>
