de20bfd3e746e0eddfb88246501ec606ef3a467c
Your Name <lazaro.domiciano@gmail.com>
2025-06-18 10:32:30 -0300

    fix: Corrige estilo da imagem do foguete na Missão.svelte

--- src/lib/components/Mission.svelte
+++ src/lib/components/Mission.svelte
@@ -25,7 +25,7 @@
         <img src="/images/earth.png" alt="Terra" class="rounded-full w-12 h-12" />
         <div class="flex flex-col items-center">
             <p>Contador: {count}/10</p>
-            <img src="/images/rocket.png" alt="Foguete" style="width: 50px; transform: translateY(-{count * 10}px);" />
+            <img src="/images/rocket.png" alt="Foguete" class="rounded-full w-12 h-12" style="transform: translateY(-{count * 10}px);" />
             <button class="btn variant-filled-primary" on:click={advanceMission}>Avançar</button>
         </div>
         <img src="/images/alien.png" alt="Planeta Alienígena" class="rounded-full w-12 h-12" />
