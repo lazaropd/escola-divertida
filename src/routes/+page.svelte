1cf27d53b79ca1192274fea700200ee72ad41786
Your Name <lazaro.domiciano@gmail.com>
2025-06-17 21:11:26 -0300

    style: Ajusta tamanho de texto e espaçamento na página inicial.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
             <img src="/images/home1.png" alt="Crianças aprendendo" class="rounded-lg w-1/4 mr-4 object-contain" />
             <div>
                 <h2 class="h2 text-primary-500 mb-2">Bem-vindo à Escola Divertida!</h2>
-                <p class="text-lg">
+                <p class="text-l">
                     Explore um mundo de aprendizado interativo e divertido! Nossa plataforma oferece atividades educativas para crianças de 6 a 15 anos.
                 </p>
             </div>
@@ -30,7 +30,7 @@
             <img src="/images/home2.png" alt="Aprendizado divertido" class="rounded-lg w-1/4 mr-4 object-contain" />
             <div>
                 <h2 class="h2 text-secondary-500 mb-2">Sobre o Projeto</h2>
-                <p class="text-lg">
+                <p class="text-l">
                     A Escola Divertida é um projeto desenvolvido para tornar o aprendizado mais acessível e envolvente para crianças. Utilizamos tecnologias modernas e conteúdos criativos para estimular o interesse e a curiosidade dos alunos.
                 </p>
             </div>
@@ -39,17 +39,17 @@
         <section class="p-8 card flex">
             <img src="/images/home3.png" alt="Aviso" class="rounded-lg w-1/4 mr-4 object-contain" />
             <div>
-                <h2 class="h2 text-tertiary-500 mb-2">Destaques Importantes e Uso Responsável</h2>
-                <p class="text-lg">
+                <h2 class="h2 text-tertiary-500 mb-4">Destaques Importantes e Uso Responsável</h2>
+                <p class="text-l">
                     Este é um projeto de desenvolvimento pessoal, sem fins lucrativos e sem qualquer vínculo com entidades governamentais, educacionais ou privadas.
                 </p>
-                <p class="text-lg">
-                    O objetivo principal é a exploração e o aprendizado através da aplicação prática de Inteligências Artificiais.
+                <p class="text-l">
+                    O objetivo principal é a exploração e o aprendizado através da aplicação prática de Inteligências Artificiais no desenvolvimento de software.
                 </p>
-                <p class="text-lg">
+                <p class="text-l">
                     O acompanhamento de um profissional capacitado da área da educação é indispensável para a formação de crianças. Este trabalho não substitui o papel da família, da sociedade e do Estado na educação.
                 </p>
-                <p class="text-lg">
+                <p class="text-l">
                     Todo o trabalho, ideias e insights gerados podem ser livremente compartilhados, desde que os créditos sejam mantidos ao criador original: Lazaro Pinheiro Domiciano.
                 </p>
             </div>
